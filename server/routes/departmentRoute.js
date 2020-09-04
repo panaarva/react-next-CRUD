110cefd4871e6b82547470a53723c7ce524f1463
parvanitakis <parvanitakis@innovative.gr>
2020-09-04 16:33:30 +0300

    routes

---
+++ server/routes/departmentRoute.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const client = require('../lib/config').pool;
+const createError = require('http-errors');
+client.connect();
+
+// GET Departments.
+router.get('/', async (req, res, next) => {
+  try {
+    const {rows} = await client.query(`SELECT *
+                                             FROM public.department`);
+    res.status(200).json(rows);
+  } catch (e) {
+    console.error(e);
+    next(createError(404, 'NOT FOUND'));
+  }
+})
+router.get('/specific/:id', async (req, res, next) => {
+  const {id} = req.params
+  try {
+    const {rows} = await client.query(`SELECT *
+                                             FROM public.department where id=$1`,[id]);
+    res.status(200).json(rows);
+  } catch (e) {
+    console.error(e);
+    next(createError(404, 'NOT FOUND'));
+  }
+})
+//ADD New Department
+router.post('/', async (req, res, next) => {
+  const {id,name} = req.body;
+  try {
+    const response = await client.query(`INSERT INTO public.department (id, name)
+                                             VALUES ($1, $2)`, [id, name]);
+    res.status(200).send("Success");
+  } catch (e) {
+    next(createError(404, 'NOT FOUND'));
+  }
+})
+//Delete Department
+router.delete('/', async (req, res, next) => {
+  const {departId} = req.query;
+  try {
+    const {rows} = await client.query(`DELETE
+                                           from public.department
+                                           where id = $1`, [departId]);
+    if (rows === 0) {
+      next(createError(404, 'INVALID USER'));
+    } else {
+      res.status(200).send('success');
+    }
+  } catch (e) {
+    console.error(e);
+    next(createError(404, 'NOT FOUND'));
+  }
+})
+//Update department
+router.put('/:depid', async (req, res, next) => {
+  const {id,name} = req.body;
+  const {depid} = req.params;
+  try {
+    const {rows} = await client.query(`UPDATE department
+                                           SET id = $3,
+                                           name = $1
+                                           where id = $2 Returning id`, [name,Number(depid),id]);
+    if (rows.length === 0) {
+      next(createError(404, 'INVALID Department'));
+    } else {
+      res.status(200);
+      res.send('success');
+    }
+  } catch (e) {
+    console.error(e);
+    next(createError(404, 'NOT FOUND'));
+  }
+})
+module.exports = router;
---
+++ server/routes/employeeRoute.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const client = require('../lib/config').pool;
+const createError = require('http-errors');
+client.connect();
+
+// GET Employee.
+router.get('/', async (req, res, next) => {
+    try {
+        const {rows} = await client.query(`SELECT *
+                                             FROM public.employee`);
+        res.status(200).send(rows);
+    } catch (e) {
+        console.error(e);
+        next(createError(404, 'NOT FOUND'));
+    }
+})
+// GET specific Employee.
+router.get('/specific', async (req, res, next) => {
+    try {
+        const {id} = req.query
+        const {rows} = await client.query(`SELECT *
+                                             FROM public.employee WHERE id = $1`, [Number(id)]);
+        res.status(200).send(rows);
+    } catch (e) {
+        console.error(e);
+        next(createError(404, 'NOT FOUND'));
+    }
+})
+//ADD New Employee
+router.post('/', async (req, res, next) => {
+    const {firstname, lastname, startDate, depId} = req.body;
+    try {
+        const response = await client.query(`INSERT INTO public.employee (fName, lName, startDate, deptId)
+                                             VALUES ($1, $2, $3, $4)`, [firstname, lastname, new Date(startDate), Number(depId)]);
+        res.status(200).send("success");
+    } catch (e) {
+        next(createError(404, 'NOT FOUND'));
+    }
+})
+//Delete Employee
+router.delete('/', async (req, res, next) => {
+    const {employeeId} = req.query;
+    try {
+        const {rows} = await client.query(`DELETE
+                                           from public.employee
+                                           where id = $1`, [employeeId]);
+        if (rows === 0) {
+            next(createError(404, 'INVALID USER'));
+        } else {
+            res.status(200).send('success');
+        }
+    } catch (e) {
+        console.error(e);
+        next(createError(404, 'NOT FOUND'));
+    }
+})
+//Update Employee
+router.put('/:id', async (req, res, next) => {
+    const {firstname, lastname, startDate, depId} = req.body;
+    const {id} = req.params;
+    try {
+        const {rows} = await client.query(`UPDATE public.employee
+                                           SET fName     = $1,
+                                               lName = $2,
+                                               startDate        = $3,
+                                               deptId       = $4
+                                           where id = $5 Returning id`, [firstname, lastname, new Date(startDate), Number(depId), Number(id)]);
+        if (rows.length === 0) {
+            next(createError(404, 'INVALID Employee'));
+        } else {
+            res.status(200);
+            res.send('success');
+        }
+    } catch (e) {
+        console.error(e);
+        next(createError(404, 'NOT FOUND'));
+    }
+})
+
+module.exports = router;
