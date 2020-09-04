a982dc4f156da4d159fe7fec24f99cb82e520e41
parvanitakis <parvanitakis@innovative.gr>
2020-09-04 16:33:21 +0300

    lib

---
+++ server/lib/config.js
@@ -0,0 +1,9 @@
+const {Pool} = require('pg');
+exports.pool = new Pool({
+    user: 'nextjs',
+    host: '127.0.0.1',
+    database: 'nextjs',
+    password: 'nextjs',
+    port: 5432
+});
+exports.privateKey = `kn0wled9e#1998`;
