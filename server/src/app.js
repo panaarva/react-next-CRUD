3a8123811fab22ac2647f7713a15099986f97b3f
parvanitakis <parvanitakis@innovative.gr>
2020-09-08 09:12:48 +0300

    app

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const EmployeeRoute = require('../routes/employeeRoute');
 const DepartmentRoute = require('../routes/departmentRoute');
+
 const cors = require('cors');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,17 +14,17 @@ app.use("/employee", EmployeeRoute);
 app.use("/department", DepartmentRoute);
 // error handler
 app.use(function (err, req, res, next) {
-  try {
-    res.status(err.status || 500);
+    try {
+        res.status(err.status || 500);
 
-    res.json({
-      status: err.status,
-      type: err.message
-    });
-    next();
-  } catch (e) {
-    console.log(e);
-  }
+        res.json({
+            status: err.status,
+            type: err.message
+        });
+        next();
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 module.exports = app;
