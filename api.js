756828466fd1442f34f220d2278be10079c9f2a7
parvanitakis <parvanitakis@innovative.gr>
2020-09-11 16:43:37 +0300

    api

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const express = require('./server/src/app');
 const http = require('http');
-const port = process.env.PORT || '80';
+const port = process.env.PORT || '9000';
 const dev = process.env.NODE_ENV !== 'production';
 const next = require('next');
 const { parse } = require('url')
@@ -9,9 +9,17 @@ const app = next({dev, dir: '../'});
 const handle = app.getRequestHandler();
 express.get('*', (req, res) => {
     const parsedUrl = parse(req.url, true)
-    return handle(req, res, parsedUrl);
+    const { pathname, query } = parsedUrl
+    if (pathname === '/employee') {
+        return app.render(req, res, '/employee', query)
+    } else if (pathname === '/') {
+        return app.render(req, res, '/', query)
+    } else {
+        return handle(req, res, parsedUrl)
+    }
 });
 app.prepare().then(() => {
     const server = http.createServer(express);
+    console.log(port)
     server.listen(port);
 });
