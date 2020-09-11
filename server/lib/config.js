87ec71a7cb707288c729f8175811fea6f6b0eaab
parvanitakis <parvanitakis@innovative.gr>
2020-09-11 17:03:59 +0300

    config

--- server/lib/config.js
+++ server/lib/config.js
@@ -1,10 +1,6 @@
 
 const {Pool} = require('pg');
 exports.pool = new Pool({
-    user: 'nextjs',
-    host: '127.0.0.1',
-    database: 'nextjs',
-    password: 'nextjs',
-    port: 5432
+    connectionString: 'postgres://rvxfsqlulhpphm:da2bfe91bfdb0f7e197624559651475678b11d4d2d6604f11c252bd975a793f8@ec2-52-48-65-240.eu-west-1.compute.amazonaws.com:5432/d6u5k9nv28q1v6'
 });
 exports.privateKey = `kn0wled9e#1998`;
