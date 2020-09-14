dc6ca4247fa14277f665e0b54a463d210f9a28dd
parvanitakis <parvanitakis@innovative.gr>
2020-09-14 11:10:45 +0300

    pages

--- pages/departments/new.js
+++ pages/departments/new.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import FormDepartment from "../../Components/FormDepartment";
 
-export default function AddEmployee() {
-
+export default function AddEmployee({localhost}) {
+    console.log(localhost);
     return (
-            <FormDepartment flag={false}/>
+        <FormDepartment flag={false} localhost={localhost}/>
     )
+}
+export const getServerSideProps = async ({req}) => {
+    const localhost = req.headers.host;
+    console.log(req.headers.host);
+    return {props: {localhost}}
 }
